07406b8b208b71a14b64e1cb7871a1f6ed9e01f4
Rakesh Kumar Swain <rakeshkumar.s@ivymobility.com>
2024-07-28 21:48:08 +0530

    initial commit

---
+++ jupiterfile.py
@@ -0,0 +1,7 @@
+
+# Open the file
+with open('/D:/BarberShop/jupiterfile.py', 'r') as file:
+    # Read the content of the file
+    content = file.read()
+    # Print the content
+    print(content)
